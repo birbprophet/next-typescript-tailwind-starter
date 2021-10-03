8562abde1eae8b7869792e502ee78347ef8de115
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-10-03 16:46:19 +0800

    added libraries

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      ...defaultTheme.colors,
+      transparent: "transparent",
+      current: "currentColor",
+      primary: { ...colors.sky, 650: "#0377B4" },
+      gray: {
+        ...colors.blueGray,
+        850: "#172033",
+        750: "#293548",
+        350: "#B0BCCD",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        serif: ["Domine", ...defaultTheme.fontFamily.serif],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-plugin-fancy"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
